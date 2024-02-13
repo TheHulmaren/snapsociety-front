2ab5a45c12deab3fa9f51141895498d110d9fb60
TheHulmaren <rbalsdldi@naver.com>
2024-02-13 14:17:09 +0900

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig, loadEnv } from "vite";
+import 'dotenv/config';
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
     plugins: [vue()],
