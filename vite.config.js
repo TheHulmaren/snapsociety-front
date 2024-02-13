6bcf8bfb6bfea89c75e8abba2b84c3f9eef144d4
TheHulmaren <rbalsdldi@naver.com>
2024-02-13 14:10:03 +0900

    asdasd

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
-  if(mode === "development" || mode === "test") {
-    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
-  }
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return defineConfig({
     plugins: [vue()],
